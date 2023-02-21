ac4d4a71d67c031d55199b0e48dff4078b2d3808
{Wggay-Music} <{bbbgreatfulsound@gmail.com}>
2023-02-21 23:08:10 +0900

    int test

---
+++ Variable.java
@@ -0,0 +1,9 @@
+class Variable{
+    public static void maim(String[] args){
+        int month;
+        month = 2;
+        int day = 21;
+        System.out.println("このチャンネルは");
+        System.out.println(month + "月"+ day + "日に公開しました");
+    }
+}
